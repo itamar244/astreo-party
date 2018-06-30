9f4e3d4d203eb398351d18dc9fbf240ec3d86a8e
itamar244 <itamary24@gmail.com>
2018-06-30 14:20:50 +0300

    update config

--- packages/app/webpack.config.js
+++ packages/app/webpack.config.js
@@ -1,34 +1,43 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MinifyPlugin = require('babel-minify-webpack-plugin');
 
-module.exports = {
-	mode: 'development',
-	entry: path.resolve(__dirname, 'src/index.ts'),
+module.exports = (mode = 'development') => {
+	const config = {
+		mode,
+		entry: path.resolve(__dirname, 'src/index.ts'),
 
-	devtool: 'source-map',
+		devtool: mode === 'development' && 'source-map',
 
-	module: {
-		rules: [
-			{
-				test: /\.ts$/,
-				use: 'ts-loader',
-			},
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
-			},
-		],
-	},
-	resolve: {
-		extensions: ['.ts', '.js'],
-		alias: {
-			shared: path.resolve(__dirname, '../shared/'),
+		module: {
+			rules: [
+				{
+					test: /\.ts$/,
+					use: 'ts-loader',
+				},
+				{
+					test: /\.css$/,
+					use: ['style-loader', 'css-loader'],
+				},
+			],
 		},
-	},
+		resolve: {
+			extensions: ['.ts', '.js'],
+			alias: {
+				shared: path.resolve(__dirname, '../shared/'),
+			},
+		},
 
-	plugins: [
-		new HtmlWebpackPlugin({
-			template: path.resolve(__dirname, 'index.html'),
-		}),
-	],
+		plugins: [
+			new HtmlWebpackPlugin({
+				template: path.resolve(__dirname, 'index.html'),
+			}),
+		],
+	};
+
+	if (mode === 'production') {
+		config.plugins.push(new MinifyPlugin());
+	}
+
+	return config;
 };
